40a65480bd9c8fe867039b5733539b5599c91dca
Fabian Badini <71156995+fbadini2@users.noreply.github.com>
2021-11-18 21:05:34 -0300

    creating and accessing sqlite database

--- App.js
+++ App.js
@@ -2,21 +2,43 @@ import "react-native-gesture-handler";
 import React from "react";
 import { View, StyleSheet, ImageBackground } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import SQLite from 'react-native-sqlite-storage';
 
-
+// create the sqlite database using opendatabase method, 
+// this method is also valid for access the previously created database
+const db = SQLite.openDatabase(
+  {
+    name: 'StorageDB',
+    location: 'default'
+  },
+  () => { },
+  error => { console.log(error) }
+);
 
 export default function App() {
   return (
-    
-      <ImageBackground
-        source={require("./assets/splash.png")}
-        style={styles.image}
-      >
-        <AppNavigator />
-      </ImageBackground>
-   
+
+    <ImageBackground
+      source={require("./assets/splash.png")}
+      style={styles.image}
+    >
+      <AppNavigator />
+    </ImageBackground>
+
   );
 }
+
+// we call function createtable to create the cities table if don't exist
+const createTable = () => {
+  db.transaction((sql) => {
+    sql.executeSql(
+      "CREATE TABLE IF NOT EXISTS "
+      + "cities "
+      + "(id INTEGER PRIMARY KEY AUTOINCREMENT, city TEXT, idApi INTEGER);"
+    )
+  })
+}
+
 
 const styles = StyleSheet.create({
   image: {
